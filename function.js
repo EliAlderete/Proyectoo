f796d4e4a9b3834f64cc4e5c497509e576996a92
EliAlderete <123274296+EliAlderete@users.noreply.github.com>
2023-01-24 12:41:19 -0300

    Proyecto Ticmas
    curriculum vitae

---
+++ function.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function() {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Habilidades en investigación, trabajo en equipo, ofimatica, entre otros."
+}
